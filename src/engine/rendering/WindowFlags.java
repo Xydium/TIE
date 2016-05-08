21094e431fa85f15b4cedb32cc7e059d7781350c
Tim Hornick (AKA Xydium) <prodriver54@gmail.com>
2016-05-07 20:44:29 -0400

    Log config added to WindowFlags

--- src/engine/rendering/WindowFlags.java
+++ src/engine/rendering/WindowFlags.java
@@ -1,5 +1,8 @@
 package engine.rendering;
 
+import engine.utility.Log;
+import engine.utility.Log.LogLevel;
+
 /**
  * Flags for the creation of a window, meant to be
  * given to the Window class for window creation
@@ -15,16 +18,21 @@ public class WindowFlags
 	
 	public WindowFlags()
 	{
-		title = "Window";
-		width = 800;
-		height = 600;
+		this("Window", 800, 600);
 	}
 	
 	public WindowFlags(String title, int width, int height)
 	{
+		this(title, width, height, LogLevel.NONE, false);
+	}
+	
+	public WindowFlags(String title, int width, int height, LogLevel logLevel, boolean consoleEnabled) 
+	{
 		this.title = title;
 		this.width = width;
 		this.height = height;
+		Log.logLevel = logLevel;
+		Log.consoleEnabled = consoleEnabled;
 	}
 	
 	public void setTitle(String title)
@@ -42,6 +50,16 @@ public class WindowFlags
 		this.height = height;
 	}
 	
+	public void setLogLevel(LogLevel logLevel) 
+	{
+		Log.logLevel = logLevel;
+	}
+	
+	public void setConsoleEnabled(boolean consoleEnabled) 
+	{
+		Log.consoleEnabled = consoleEnabled;
+	}
+	
 	public String getTitle()
 	{
 		return title;
@@ -56,4 +74,15 @@ public class WindowFlags
 	{
 		return height;
 	}
+	
+	public LogLevel getLogLevel()
+	{
+		return Log.logLevel;
+	}
+	
+	public boolean getConsoleEnabled()
+	{
+		return Log.consoleEnabled;
+	}
+	
 }
